d836eed36497bf778fb642693972499dc3e79c04
Marina Ermakova <m.ermakova77@gmail.com>
2015-03-24 15:06:49 +0300

    move insert to separate file

--- database.c
+++ database.c
@@ -1,6 +1,6 @@
 #include "database.h"
 
-// TODO: close database, delete key, insert key, select key, insert_nonfull
+// TODO: close database, delete key, select key
 
 //+----------------------------------------------------------------------------+
 //| Close database                                                             |
@@ -21,38 +21,6 @@ int f_delete(DB *db, DBT *key) {
     /* Check params */
     assert(db && db->info && db->root);
     assert(key && key->data);
-
-    return 0;
-}
-
-//+----------------------------------------------------------------------------+
-//| Insert key-value                                                           |
-//+----------------------------------------------------------------------------+
-
-int f_insert(DB *db, DBT *key, DBT *value) {
-    /* Check params */
-    assert(db && db->info && db->root);
-    assert(key && key->data && value && value->data);
-
-    /* Find place for key */
-    block *current = db->root;
-
-    /* Compute free memory in block and needed memory */
-    size_t free_mem = free_space(current, db->info->block_size);
-    size_t needed_mem = sizeof(size_t) * 2 + key->size + value->size;
-    if (current->num_children > 0)
-        needed_mem += 8;
-
-    /* Compare them */
-    if (needed_mem <= free_mem)
-        return insert_nonfull(db, current, db->info->root_index, key, value);
-
-    /* Root is full -> split root */
-    // s = allocate node
-    // root = s
-    // s.child = current
-    // split child(s)
-    // return insert_nonfull(s, key, value)
 
     return 0;
 }
@@ -101,55 +69,4 @@ int f_sync(DB *db) {
     assert(db && db->info && db->root);
 
     return 0;
-}
-
-//+----------------------------------------------------------------------------+
-//| Helpful functions                                                          |
-//+----------------------------------------------------------------------------+
-
-int insert_nonfull(DB *db, block *b, size_t k, DBT *key, DBT *value) {
-    /* Check params */
-    assert(db && db->info && db->root);
-    assert(key && key->data && value && value->data);
-    assert(k >= db->info->first_node && k <= db->info->num_blocks);
-
-    /* Insert key into this block or one of its children */
-    if (b->num_children == 0) {
-        /* Leaf block */
-    } else {
-        /* Non-leaf block */
-    }
-
-    /*write(1, key->data, key->size);
-    printf("\n");
-    write(1, value->data, value->size);
-    printf("\n");*/
-
-    return 0;
-}
-
-size_t free_space(block *b, size_t block_size) {
-    /* Check params */
-    assert(b);
-    assert(b->items || b->num_keys == 0);
-    assert(b->children || b->num_children == 0);
-
-    /* Busy space: */
-    /* 1. sizeof(num_keys + num_children) */
-    size_t busy = sizeof(size_t) * 2;
-
-    for (int i = 0; i < b->num_keys; ++i) {
-        /* 2. sizeof(key->size + value-> size) */
-        busy += sizeof(size_t) * 2;
-
-        /* 3. key->size */
-        busy += b->items[i]->key->size;
-
-        /* 4. value->size */
-        busy += b->items[i]->value->size;
-    }
-    /* 5. num_children * sizeof(child) */
-    busy += b->num_children * sizeof(size_t);
-
-    return block_size - busy;
 }
--- database.h
+++ database.h
@@ -2,6 +2,7 @@
 #define __DATABASE_H__
 
 #include "classes.h"
+#include "helpful.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,15 +18,7 @@
 
 int f_close(DB *db);
 int f_delete(DB *db, DBT *key);
-int f_insert(DB *db, DBT *key, DBT *value);
 int f_select(DB *db, DBT *key, DBT *value);
 int f_sync(DB *db);
-
-//+----------------------------------------------------------------------------+
-//| Helpful functions                                                          |
-//+----------------------------------------------------------------------------+
-
-int    insert_nonfull(DB *db, block *b, size_t k, DBT *key, DBT *value);
-size_t free_space(block *b, size_t block_size);
 
 #endif /* __DATABASE_H__ */
